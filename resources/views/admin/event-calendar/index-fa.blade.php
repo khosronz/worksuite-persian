1247ad1762e12b3299c3027fe0a7934fac15f651
khosro nazari <khosronz.com@gmail.com>
2020-05-30 10:04:23 +0430

    Persian lang to events

--- resources/views/admin/event-calendar/index-fa.blade.php
+++ resources/views/admin/event-calendar/index-fa.blade.php
@@ -184,12 +184,216 @@
     </div>
     <!-- .row -->
 
+
+
     <!-- BEGIN MODAL -->
+    <div class="modal fade bs-modal-md in" id="my-event" role="dialog" aria-labelledby="myModalLabel"
+         aria-hidden="true">
+        <div class="modal-dialog modal-lg" id="modal-data-application">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title"><i class="icon-plus"></i> @lang('modules.events.addEvent')</h4>
+                </div>
+                <div class="modal-body">
+                    {!! Form::open(['id'=>'createEvent','class'=>'ajax-form','method'=>'POST']) !!}
+                    <div class="form-body">
+                        <div class="row">
+                            <div class="col-md-6 ">
+                                <div class="form-group">
+                                    <label class="required">@lang('modules.events.eventName')</label>
+                                    <input type="text" name="event_name" id="event_name" class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="col-md-2 ">
+                                <div class="form-group">
+                                    <label>@lang('modules.sticky.colors')</label>
+                                    <select id="colorselector" name="label_color">
+                                        <option value="bg-info" data-color="#5475ed" selected>Blue</option>
+                                        <option value="bg-warning" data-color="#f1c411">Yellow</option>
+                                        <option value="bg-purple" data-color="#ab8ce4">Purple</option>
+                                        <option value="bg-danger" data-color="#ed4040">Red</option>
+                                        <option value="bg-success" data-color="#00c292">Green</option>
+                                        <option value="bg-inverse" data-color="#4c5667">Grey</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="col-md-4 ">
+                                <div class="form-group">
+                                    <label class="required">@lang('modules.events.where')</label>
+                                    <input type="text" name="where" id="where" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-xs-12 ">
+                                <div class="form-group">
+                                    <label class="required">@lang('app.description')</label>
+                                    <textarea name="description" id="description" class="form-control"></textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-12 col-md-3 ">
+                                <div class="form-group">
+                                    <label class="required">@lang('modules.events.startOn')</label>
+                                    <input type="text" name="start_date" id="start_date" class="form-control" autocomplete="none">
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-md-3">
+                                <div class="input-group bootstrap-timepicker timepicker">
+                                    <label>&nbsp;</label>
+                                    <input type="text" name="start_time" id="start_time"
+                                           class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="col-xs-12 col-md-3">
+                                <div class="form-group">
+                                    <label class="required">@lang('modules.events.endOn')</label>
+                                    <input type="text" name="end_date" id="end_date" class="form-control" autocomplete="none">
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-md-3">
+                                <div class="input-group bootstrap-timepicker timepicker">
+                                    <label>&nbsp;</label>
+                                    <input type="text" name="end_time" id="end_time"
+                                           class="form-control">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-12"  id="attendees">
+                                <div class="form-group">
+                                    <label class="col-xs-3 m-t-10 required">@lang('modules.events.addAttendees')</label>
+                                    <div class="col-xs-7">
+                                        <div class="checkbox checkbox-info">
+                                            <input id="all-employees" name="all_employees" value="true"
+                                                   type="checkbox">
+                                            <label for="all-employees">@lang('modules.events.allEmployees')</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <select class="select2 m-b-10 select2-multiple " multiple="multiple"
+                                            data-placeholder="@lang('modules.messages.chooseMember'), @lang('modules.projects.selectClient')" name="user_id[]">
+                                        @foreach($employees as $emp)
+                                            <option value="{{ $emp->id }}">{{ ucwords($emp->name) }} @if($emp->id == $user->id)
+                                                    (YOU) @endif</option>
+                                        @endforeach
+                                    </select>
+
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="row">
+                            <div class="form-group">
+                                <div class="col-xs-6">
+                                    <div class="checkbox checkbox-info">
+                                        <input id="repeat-event" name="repeat" value="yes"
+                                               type="checkbox">
+                                        <label for="repeat-event">@lang('modules.events.repeat')</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row" id="repeat-fields" style="display: none">
+                            <div class="col-xs-6 col-md-3 ">
+                                <div class="form-group">
+                                    <label>@lang('modules.events.repeatEvery')</label>
+                                    <input type="number" min="1" value="1" name="repeat_count" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-xs-6 col-md-3">
+                                <div class="form-group">
+                                    <label>&nbsp;</label>
+                                    <select name="repeat_type" id="" class="form-control">
+                                        <option value="day">@lang('app.day')</option>
+                                        <option value="week">@lang('app.week')</option>
+                                        <option value="month">@lang('app.month')</option>
+                                        <option value="year">@lang('app.year')</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="col-xs-6 col-md-3">
+                                <div class="form-group">
+                                    <label>@lang('modules.events.cycles') <a class="mytooltip" href="javascript:void(0)"> <i class="fa fa-info-circle"></i><span class="tooltip-content5"><span class="tooltip-text3"><span class="tooltip-inner2">@lang('modules.events.cyclesToolTip')</span></span></span></a></label>
+                                    <input type="text" name="repeat_cycles" id="repeat_cycles" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="form-group">
+                                <div class="col-xs-6">
+                                    <div class="checkbox checkbox-info">
+                                        <input id="send_reminder" name="send_reminder" value="yes"
+                                               type="checkbox">
+                                        <label for="send_reminder">@lang('modules.tasks.reminder')</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row" id="reminder-fields" style="display: none;">
+                            <div class="col-xs-6 col-md-3">
+                                <div class="form-group">
+                                    <label>@lang('modules.events.remindBefore')</label>
+                                    <input type="number" min="1" value="1" name="remind_time" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-xs-6 col-md-3">
+                                <div class="form-group">
+                                    <label>&nbsp;</label>
+                                    <select name="remind_type" id="" class="form-control">
+                                        <option value="day">@lang('app.day')</option>
+                                        <option value="hour">@lang('app.hour')</option>
+                                        <option value="minute">@lang('app.minute')</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {!! Form::close() !!}
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-white waves-effect" data-dismiss="modal">@lang('app.close')</button>
+                    <button type="button" class="btn btn-success save-event waves-effect waves-light">@lang('app.submit')</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
     {{--Ajax Modal--}}
-
+    <div class="modal fade bs-modal-md in" id="eventDetailModal" role="dialog" aria-labelledby="myModalLabel"
+         aria-hidden="true">
+        <div class="modal-dialog modal-lg" id="modal-data-application">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                    <span class="caption-subject font-red-sunglo bold uppercase" id="modelHeading"></span>
+                </div>
+                <div class="modal-body">
+                    Loading...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn default" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn blue">Save changes</button>
+                </div>
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
     {{--Ajax Modal Ends--}}
-
 @endsection
 
 @push('footer-script')
